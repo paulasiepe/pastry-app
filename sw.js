85e203224a159d344703fd7a08c1b09c39a3d5ca
paulasiepe <paulasiepe@gmail.com>
2026-04-18 19:31:13 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C='pastry-v6',U=['/','/index.html','/manifest.json','/icons/icon-192x192.png','/icons/icon-512x512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(U).catch(()=>{})));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('/'))));});
