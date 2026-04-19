4871268b71e6f9d76659d6f4d0e227c84891ca04
paulasiepe <paulasiepe@gmail.com>
2026-04-19 16:12:35 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const C='pastry-v1776625212',U=['/','/index.html','/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(U).catch(()=>{})));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).catch(()=>caches.match('/'))));})
